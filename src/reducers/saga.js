7727f9d8a9d60b5406ee41ca907eba631e6a0f9e
Viet Do <viet.do@saleshood.com>
2023-09-23 11:39:43 +0700

    remove redux saga firestore

--- src/reducers/saga.js
+++ src/reducers/saga.js
@@ -11,16 +11,10 @@ import {
   BEGIN_FETCH_TASK,
   BEGIN_DELETE_TASK,
 } from "../actions/types";
-import rsf from "../firestore";
 
 function* fetchTask({ onSuccess }) {
   try {
-    const snapshot = yield call(rsf.firestore.getCollection, "tasks");
     const tasks = [];
-    snapshot.forEach((doc) => {
-      const data = doc.data();
-      tasks.push({ id: doc.id, completed: data.completed, title: data.title });
-    });
     onSuccess(true);
     yield put(finishFetchTasks(tasks));
   } catch (e) {
@@ -34,7 +28,6 @@ function* addTask(action) {
       title: action.payload,
       completed: false,
     };
-    yield call(rsf.firestore.addDocument, "tasks", newTask);
     yield put(finishAddTask(newTask));
   } catch (e) {
     console.error(e);
@@ -43,19 +36,7 @@ function* addTask(action) {
 
 function* completeTask(action) {
   try {
-    yield call(
-      rsf.firestore.updateDocument,
-      `tasks/${action.payload.id}`,
-      "completed",
-      !action.payload.completed
-    );
-    const snapshot = yield call(rsf.firestore.getCollection, "tasks");
     const tasks = [];
-    snapshot.forEach((doc) => {
-      const data = doc.data();
-      tasks.push({ id: doc.id, completed: data.completed, title: data.title });
-    });
-
     yield put(finishCompleteTask(tasks));
   } catch (e) {
     console.error(e);
@@ -65,15 +46,7 @@ function* completeTask(action) {
 function* deleteTask(action) {
   const id = action.payload;
   try {
-    yield call(rsf.firestore.deleteDocument, `tasks/${id}`);
-
-    const snapshot = yield call(rsf.firestore.getCollection, "tasks");
     const tasks = [];
-    snapshot.forEach((doc) => {
-      const data = doc.data();
-      tasks.push({ id: doc.id, completed: data.completed, title: data.title });
-    });
-
     yield put(finishDeleteTask(tasks));
   } catch (error) {
     console.error(error);
